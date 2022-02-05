f91b470e4844d8e9becbd03505963f42e8845b54
hebin <97599515+hebin86010@users.noreply.github.com>
2022-02-06 00:25:54 +0800

    Update index.js
    修改了express的入口文件index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 // 0. 引入第三方库
 const express = require("express")
-// const {rizhiM, notFoundMF, handlerErrorMF} = require("./middlewares/baseMiddl")  //引入日志中间件
-const { crossDomainM, rizhiM, err404MF, err500MF} = require("./middlewares/baseMiddl")  //引入日志中间件
-// const dayjs = require("dayjs")    //第三方时间插件dayjs
+// const {rizhiM, notFoundMF, handlerErrorMF} = require("./middlewares/baseMiddl")       //引入日志中间件
+const { crossDomainM, rizhiM, err404MF, err500MF} = require("./middlewares/baseMiddl")   //引入日志中间件
 const path = require("path")
-// const apiRouter = require("./api/v1/index")  //引入封装的api组件
+// const apiRouter = require("./api/v1/index")                                           //引入封装的api组件,这个地方是否可以把版本号'v1'改为变量，这样可以数据库中取值
+
+//引入和定义常量
+const port = 80                             //express监听的服务器端口
+const page404 = "./defaulPages/404.html"    //404错误响应页面 , 可以根据后台配置不同风格的响应页面
+const page500 = "./defaulPages/500.html"    //500错误响应页面 , 可以根据后台配置不同风格的响应页面
 
 // 1. 创建一个应用服务器
 const app = express()  
-const port = 80       //监听的服务器端口
 
 // 2.挂载工具中间件
 app.use(express.json(), express.urlencoded({extended:true}))  //post请求参数预处理处理中间件：son格式、普通的&拼接
@@ -19,8 +22,7 @@ app.use(express.static(path.resolve(__dirname,"public")))    //静态资源服�
 
 
 //3 挂载路由中间件
-//api中间件 
-// app.use("/api", apiRouter)
+// app.use("/api", apiRouter)                               //api中间件 
 app.all('/api', (req, resp)=> {
   // 错误代码测试
   // abc()
@@ -30,14 +32,10 @@ app.all('/api', (req, resp)=> {
 })
 
 // 4 挂载错误中间件
-// 4.1 404中间件
-//前面的路由，都没有匹配到
-let page404 = "./defaulPages/404.html" //404错误响应页面 , 可以根据后台设置、调整响应页面
+//404中间件：前面的路由，都没有匹配到
 app.use(err404MF(path.resolve(__dirname, page404)))
 
-// 4.2 500错误处理中间件
-// 500错误处理中间件放在最后的位置
-let page500 = "./defaulPages/500.html"  //500错误响应页面 , 可以根据后台设置、调整响应页面
+// 500错误中间件：放在最后的位置
 app.use(err500MF(path.resolve(__dirname, page500)))
 
 // 5 启动服务器
